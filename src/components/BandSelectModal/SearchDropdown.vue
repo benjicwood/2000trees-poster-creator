e4e0ef37834e3e989254194a05e38a6cb861a186
benjicwood <bencharlwood@gmail.com>
2025-07-31 00:11:43 +0100

    missing logos

--- src/components/BandSelectModal/SearchDropdown.vue
+++ src/components/BandSelectModal/SearchDropdown.vue
@@ -55,7 +55,7 @@ export default {
         maxItem: {
             type: Number,
             required: false,
-            default: 700,
+            default: 750,
         },
         id: {
             type: Number,
