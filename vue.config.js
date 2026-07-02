95cab2c7fc4a03debf1aeb1b843efd3587fa2af3
benjicwood <bencharlwood@gmail.com>
2026-07-02 21:31:42 +0100

    Use custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/2000trees-poster-creator/" : "/",
-})
+  publicPath: "/",
+});
